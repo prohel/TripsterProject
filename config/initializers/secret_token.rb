8996beab8aae6ca885fd7c0576e8a236ecef9b0b
pierre <pierrerohel@gmail.com>
2014-12-08 23:30:13 -0500

    starting all over again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripsterProject::Application.config.secret_token = 'a89aff9fb2fb77eddfb9bfa89afd7bae0a14fa711aadc08782de72b567bbc67ac78794e61f6a064e361bf667c5e63c95f972dd34b25d09c22aaafd159871da0b'
